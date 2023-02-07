4369a080b016b646b827f498ddfb16f0ec24fc7b
Rodrigo Noblega <rodrigonoblega@Rodrigos-MacBook-Pro.local>
2023-02-07 18:06:19 -0300

    Implementation with only 10 images requested

--- main.go
+++ main.go
@@ -1,24 +1,18 @@
 package main
 
 import (
-	"flag"
 	"github.com/rodrinoblega/proper_images_processor/src/adapters"
 	"github.com/rodrinoblega/proper_images_processor/src/configuration"
 	"github.com/rodrinoblega/proper_images_processor/src/instrumentation"
-	"strconv"
 	"time"
 )
 
 func main() {
-	amountOfImagesRequested := flag.Int("amount", 1, "an int")
-	flag.Parse()
-	instrumentation.LogMessage("Amount of images to find: " + strconv.Itoa(*amountOfImagesRequested))
-
 	start := time.Now()
 
 	service := adapters.NewImagesProcessorService(configuration.CreateImagesProcessorService())
 
-	error := service.Execute(*amountOfImagesRequested)
+	error := service.Execute(10)
 	if error != nil {
 		instrumentation.LogMessage("The application has the following error: " + error.Error())
 	}
